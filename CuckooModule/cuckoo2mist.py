8d3069e8f671feaee6acf1720fe8fb32ecaefcaf
Greg <greg@dgse>
2017-06-01 11:15:19 +0200

    Added Cukoo module

---
+++ CuckooModule/cuckoo2mist.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import os
+import json
+import codecs
+from pathlib import Path
+from lib.cuckoo.common.abstracts import Report
+from lib.cuckoo.common.exceptions import CuckooReportError
+
+
+
+class cuckoo2mist(Report):
+	order=2
+	# Enregistre le rapport au format Mist
+	def run(self, results):
+		print("Lecture du JSON")
+		path = os.path.join(self.reports_path, "report.json")
+		reportJSON = Path(path)
+		if reportJSON.is_file():
+			# Get script path in config
+			CuckooConvert = self.options.get("script", "/")	
+			os.system(CuckooConvert+"cuckoo2mist.py -i"+self.reports_path)
+			print("Conversion terminée !")
+
+
+
+
